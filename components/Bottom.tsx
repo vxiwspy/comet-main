6bc03cb0233b32453be10a5c69342d08c5c623ae
supoti679 <supoti679@gmail.com>
2024-10-10 08:06:08 +0000

    bottom navbar

--- components/Bottom.tsx
+++ components/Bottom.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import {House, User, Apple, Hash} from 'lucide-react'
+import Link from "next/link";
+// #9f87ff
 export default function Bottom() {
   return (
     <div
-    className="md:hidden">Bottom</div>
+    className="md:hidden flex flex-inline justify-between">
+      <Link href="/" className="">
+        <House color="#fff" className='hover:fill-[#9f87ff]' size={16} strokeWidth={1.5} />
+      </Link>
+      <Link href="/" className="">
+        <Apple color="#fff" className='hover:fill-[#9f87ff]' size={16} strokeWidth={1.5} />
+      </Link>
+      <Link href="/" className="">
+        <Hash color="#fff" className='hover:fill-[#9f87ff]' size={16} strokeWidth={1.5} />
+      </Link>
+      <Link href="/" className="">
+        <User color="#fff" className='hover:fill-[#9f87ff]' size={16} strokeWidth={1.5} />
+      </Link>
+    </div>
   )
 }
