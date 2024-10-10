4adcb1e2a5f61741187217a19496520ea54f4ab1
supoti679 <supoti679@gmail.com>
2024-10-10 11:54:14 +0000

    bottom navbar for mobile view

--- components/Bottom.tsx
+++ components/Bottom.tsx
@@ -1,28 +1,31 @@
-import React from 'react'
+"use client";
+
+import React, {useState} from 'react'
 import {House, User, Apple, Hash} from 'lucide-react'
 import Link from "next/link";
 
-
 export default function Bottom() {
+
+  const [current, setCurrent] = useState("Home");
 
   return (
     <div
-    className="md:hidden flex flex-inline justify-between fixed bottom-0 w-screen bg-[#151517] rounded-t-xl ">
-      <Link href="/" className="mx-auto my-5 flex-col flex justify-center items-center">
-        <House color="#4b5563" className='flex' size={29} strokeWidth={2.5} />
-        <span className="text-gray-600 text-sm font-normal mx-2">Home</span>
+    className="md:hidden flex flex-inline justify-between fixed z-50  bottom-0 w-screen bg-[#151517] rounded-t-xl">
+      <Link href="/" className="mx-auto my-5 flex-col flex justify-center items-center rounded-lg" onClick={() => setCurrent("Home")}>
+        <House color={current === "Home" ? "#9f87ff" : "#4b5563"} className='flex' size={29} strokeWidth={2.5} />
+        <span className={`${current === "Home" ? "text-[#9f87ff]" : "text-gray-600"} text-sm font-normal mx-2`}>Home</span>
       </Link>
-      <Link href="/" className="mx-auto my-5 flex-col flex justify-center items-center">
-        <Apple color="#4b5563" className='flex' size={29} strokeWidth={2.5} />
-        <span className="text-gray-600 text-sm font-normal mx-2">Meals</span>
+      <Link href="/" className="mx-auto my-5 flex-col flex justify-center items-center" onClick={() => setCurrent("Meals")} >
+        <Apple color={current === "Meals" ? "#9f87ff" : "#4b5563"} className='flex' size={29} strokeWidth={2.5} />
+        <span className={`${current === "Meals" ? "text-[#9f87ff]" : "text-gray-600"} text-sm font-normal mx-2`}>Meals</span>
       </Link>
-      <Link href="/" className="mx-auto my-5 flex-col flex justify-center items-center">
-        <Hash color="#4b5563" className='flex' size={29} strokeWidth={2.5} />
-        <span className="text-gray-600 text-sm font-normal mx-2">Scores</span>
+      <Link href="/" className="mx-auto my-5 flex-col flex justify-center items-center" onClick={() => setCurrent("Scores")}>
+        <Hash color={current === "Scores" ? "#9f87ff" : "#4b5563"} className='flex' size={29} strokeWidth={2.5} />
+        <span className={`${current === "Scores" ? "text-[#9f87ff]" : "text-gray-600"} text-sm font-normal mx-2`}>Scores</span>
       </Link>
-      <Link href="/" className="mx-auto my-5 flex-col flex justify-center items-center">
-        <User color="#4b5563" className='flex' size={29} strokeWidth={2.5} />
-        <span className="text-gray-600 text-sm font-normal mx-2 ">Profile</span>
+      <Link href="/" className="mx-auto my-5 flex-col flex justify-center items-center" onClick={() => setCurrent("Profile")}>
+        <User color={current === "Profile" ? "#9f87ff" : "#4b5563"} className='flex' size={29} strokeWidth={2.5} />
+        <span className={`${current === "Profile" ? "text-[#9f87ff]" : "text-gray-600"} text-sm font-normal mx-2`}>Profile</span>
       </Link>
     </div>
   )
